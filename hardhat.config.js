0b5a97e546e7be8c370e71ea3a590eb01a0d5217
karangorania <karangoraniya@gmail.com>
2022-04-14 22:39:41 +0530

    Network configuration changed  🚀

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,15 @@
 require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
 
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-const { ROPSTEN_API_URL_KEY, PRIVATE_KEY } = process.env;
+const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
-  solidity: '0.8.13',
+  solidity: '0.8.4',
+  defaultNetwork: 'rinkeby',
   networks: {
-    matic: {
-      url: ROPSTEN_API_URL_KEY,
+    hardhat: {},
+    rinkeby: {
+      url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     },
   },
