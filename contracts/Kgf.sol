74ae5c6d050169f1caed59f67534dd9e42f09a57
karangorania <karangoraniya@gmail.com>
2022-04-14 15:26:27 +0530

    kgf contract added 🚀

---
+++ contracts/Kgf.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Kgf is ERC721 {
+
+    constructor() ERC721('KgfRocky','KGF') {
+        
+    }
+}
